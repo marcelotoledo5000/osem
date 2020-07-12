60d93a9a78eccb92245454c29d657b543ff7ac50
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-07-11 22:58:28 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 # Create sample user
 user = User.find_or_initialize_by(email: 'deleted@localhost.osem', name: 'User deleted',
                                   username: 'deleted_user', is_disabled: true,
